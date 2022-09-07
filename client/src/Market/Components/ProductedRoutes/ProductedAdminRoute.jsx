c36372c8a23d86108fb6ede800e2c6d78b35a1bd
mohammedramadan99 <lifeisagame0101@gmail.com>
2022-09-07 14:34:06 +0200

    s

--- client/src/Market/Components/ProductedRoutes/ProductedAdminRoute.jsx
+++ client/src/Market/Components/ProductedRoutes/ProductedAdminRoute.jsx
@@ -1,14 +1,17 @@
-import {useNavigate} from 'react-router-dom'
-import {Outlet} from 'react-router-dom'
-import {useSelector} from 'react-redux'
-const useAuth = () => {
-    const {user} = useSelector(state => state.auth.userInfo)
-    
+import { useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+const useAuth = () =>
+{
+    const { user } = useSelector(state => state.auth.userInfo)
+
     return user && user.role === 'admin'
+
 }
 
-const ProductedAdminRoute = () => {
+const ProductedAdminRoute = () =>
+{
     const isAuth = useAuth()
-    return isAuth ? <Outlet/> : <h1>only admin can access pages like this </h1>
+    return isAuth ? <Outlet /> : <h1>only admin can access pages like this </h1>
 }
 export default ProductedAdminRoute
